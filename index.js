1ee7f48ef685311d13acd2236d2f60089632e762
Gandavarapu Sai Prasad <128301186+Saiprasad437@users.noreply.github.com>
2023-05-24 11:31:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber = Math.floor(Math.random() * 100)+1;
+var attempts = 0;
+document.getElementById("guessBtn").addEventListener("click", function(){
+    var guess = parseInt(document.getElementById("guessInput").value);
+attempts++;
+
+if (guess === randomNumber){
+    displayMessage("Congratulations! you gueesed the number in"+ attempts + "attempts.");
+    document.getElementById("guessBtn").disabled=true;
+}
+else if(guess < randomNumber){
+    displayMessage("Too low! try a higher number");
+}
+else{
+    displayMessage("Two high! try a lower number.");
+}
+});
+function displayMessage(message){
+    document.getElementById("message").textContent=message;
+}
